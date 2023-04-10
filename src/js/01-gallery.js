7f5f23a4683704a6751679c44847a0952672916a
Bartosz-Zarski <117305195+Bartosz-Zarski@users.noreply.github.com>
2023-04-10 13:40:03 +0200

    Revert "task 1 test"
    This reverts commit bbacf997e900443b33342720de241fc62001b53c.

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,29 +1,5 @@
 // Add imports above this line
-import simpleLightbox from 'simplelightbox';
-import "simplelightbox/dist/simple-lightbox.min.css";
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from './gallery-items';
+// Change code below this line
 
-
-const fullGallery = document.querySelector('.gallery');
-
-// STWORZENIE ELEMENTOW W DOM I PODPIECIE ICH
-let galleryEl = galleryItems
-  .map(function (image) {
-    return `<li><a class="gallery__item" href="${image.original}">
-    <img class="gallery__image" src="${image.preview}" alt="${image.description}" />
-  </a></li>`;
-  })
-  .join('');
-
-fullGallery.insertAdjacentHTML('beforeend', galleryEl);
-
-// ZABLOKOWANIE LINKU PO KLIKNIĘCIU
-fullGallery.addEventListener('click', event => {
-  event.preventDefault();
-});
-
-let gallery = new SimpleLightbox('.gallery li a', {
-  captionsData: 'alt',
-  captionDelay: 250,
-  captionPosition: 'bottom',
-});
+console.log(galleryItems);
